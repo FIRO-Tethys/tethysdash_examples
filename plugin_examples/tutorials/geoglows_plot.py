050930f130acf48388f60450b7f0151104408754
Corey Krewson <ckrewson@aquaveo.com>
2026-05-11 13:48:05 -0500

    remove geoglows dependency because its slow

--- plugin_examples/tutorials/geoglows_plot.py
+++ plugin_examples/tutorials/geoglows_plot.py
@@ -1,10 +1,8 @@
 from tethysapp.tethysdash.plugin_helpers import TethysDashPlugin
-import plotly.express as px
-import json
-import geoglows
+import requests
 
 
-class PlotlyExample(TethysDashPlugin):
+class GeoGLOWSForecastPlot(TethysDashPlugin):
     name = "geoglows_forecast_plot"
     group = "Tutorials"
     label = "GeoGLOWS Forecast Plot"
@@ -22,6 +20,42 @@ class PlotlyExample(TethysDashPlugin):
         """
         Return plotly information
         """
-        forecast_data = geoglows.data.forecast(int(self.river_ID))
-        forecast_plot = geoglows.plots.forecast(forecast_data)
-        return json.loads(forecast_plot.to_json())
+        self.send_update("Loading forecast data from GeoGLOWS API...")
+        url = f"https://geoglows.ecmwf.int/api/v2/forecast/{self.river_ID}?format=json"
+        response = requests.get(url)
+        forecast_data = response.json()
+
+        self.send_update("Processing forecast data...")
+        data = [
+            {
+                "type": "scatter",
+                "x": forecast_data["datetime"],
+                "y": forecast_data["flow_uncertainty_lower"],
+                "name": "Lower Uncertainty",
+                "line": {"color": "lightblue"},
+            },
+            {
+                "type": "scatter",
+                "x": forecast_data["datetime"],
+                "y": forecast_data["flow_uncertainty_upper"],
+                "name": "Upper Uncertainty",
+                "line": {"color": "lightblue"},
+                "fill": "tonexty",
+                "fillcolor": "lightblue",
+            },
+            {
+                "type": "scatter",
+                "x": forecast_data["datetime"],
+                "y": forecast_data["flow_median"],
+                "name": "Median Forecast",
+                "line": {"color": "darkblue"},
+            },
+        ]
+
+        layout = {
+            "title": f"GeoGLOWS Forecast ({self.river_ID})",
+        }
+
+        config = {"displayModeBar": True}
+
+        return {"data": data, "layout": layout, "config": config}
